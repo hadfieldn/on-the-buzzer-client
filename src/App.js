e0d81f60839cd28ceff8ea314a6a4ae1afd6c679
Nathan Hadfield <hadfieldn@gmail.com>
2020-03-09 14:41:05 -0600

    add dogs

--- src/App.js
+++ src/App.js
@@ -22,11 +22,19 @@ class App extends Component {
   }
 
   onMessage(data) {
+    console.log({data: data})
     if (data.event === EVENTS.BUZZ) {
+      const button = data.payload.button
       this.setState({ buzzed: data.payload.button });
     } else if (data.event === EVENTS.QUESTION_RESETTED) {
+      this.playSound(button === 1 ? "small-dog-bark" : "big-dog-bark");
       this.setState({ buzzed: undefined });
     }
+  }
+
+  playSound(soundId) {
+    const sound = document.getElementById(soundId)
+    sound.play();
   }
 
   reset() {
@@ -36,6 +44,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <audio id="buzzer-audio" src="/beep4.wav" autoPlay={false} />
+        <audio id="big-dog-bark" src="/big_dog_bark.wav" autoPlay={false} />
+        <audio id="small-dog-bark" src="/small_dog_bark.m4a" autoPlay={false} />
         <div className="Result">
           <div
             className={`team-1 ${
@@ -45,7 +56,9 @@ class App extends Component {
                   : "not-buzzed"
                 : ""
             }`}
-          ></div>
+          >
+            <img src="/small_dog.png" alt={"small dog"}/>
+          </div>
           <div
             className={`team-2 ${
               this.state.buzzed
@@ -54,7 +67,9 @@ class App extends Component {
                   : "not-buzzed"
                 : ""
             }`}
-          ></div>
+          >
+            <img src="/big_dog.png" alt={"big dog"}/>
+          </div>
         </div>
       </div>
     );
